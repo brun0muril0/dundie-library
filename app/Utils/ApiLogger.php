3694f4bcd7b65986261f16fd3783ae06b85c4574
Bruno Murilo <brunomuriloc014@hotmail.com>
2023-08-31 11:53:01 -0300

    :sparkles: Feat: log viewer creation

---
+++ app/Utils/ApiLogger.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Utils;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+
+class ApiLogger
+{
+    public static function info(string $class, string $method, string $message)
+    {
+        Log::info('[' . $class . '][' . $method . '][' . $message . ']');
+    }
+
+    public static function error(string $class, string $method, string $message)
+    {
+        Log::error('[' . $class . '][' . $method . '][' . $message . ']');
+    }
+
+    public static function exception(string $class, string $method, Exception $error)
+    {
+        Log::error('[' . $class . '][' . $method . '][Linha: ' . $error->getLine() . '][Erro: ' . $error->getMessage() . ']');
+    }
+
+    public static function debug(string $class, string $method, string $message)
+    {
+        Log::debu('[' . $class . '][' . $method . '][' . $message . ']');    }
+}
