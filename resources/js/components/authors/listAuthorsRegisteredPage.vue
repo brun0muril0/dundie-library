7bef640f23c4728fc243315b1c9842a2a7a4ca88
Bruno Murilo <brunomuriloc014@hotmail.com>
2023-08-31 16:36:57 -0300

    :recycle: refactor: words corrections

--- resources/js/components/authors/listAuthorsRegisteredPage.vue
+++ resources/js/components/authors/listAuthorsRegisteredPage.vue
@@ -138,7 +138,7 @@
                                         </div>
                                         <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                             <button type="submit" class="inline-flex w-full justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:hover:bg-blue-600 sm:ml-3 sm:w-auto">Salvar</button>
-                                            <button type="cancel" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="editModal = false">cancelar</button>
+                                            <button type="cancel" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="editModal = false">Cancelar</button>
                                         </div>
                                     </form>
                                 </div>
--- resources/js/components/books/listBooksRegisteredPage.vue
+++ resources/js/components/books/listBooksRegisteredPage.vue
@@ -144,7 +144,7 @@
                                             <label for="publication" class="block text-sm font-medium leading-6 text-gray-900">Ano de publicação</label>
                                             <div class="mt-2">
                                                 <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-bg-blue-500 sm:max-w-md">
-                                                <input v-model="selectedItem.publication" type="date" name="publication" id="publication" autocomplete="publication" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" required/>
+                                                <input v-model="selectedItem.publication" type="number" name="publication" id="publication" autocomplete="publication" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" required/>
                                                 </div>
                                             </div>
                                         </div>
@@ -154,7 +154,7 @@
                         </div>
                         <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button type="button" class="inline-flex w-full justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:hover:bg-blue-600 sm:ml-3 sm:w-auto" @click="updateBook">Salvar</button>
-                            <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="editModal = false">cancelar</button>
+                            <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="editModal = false">Cancelar</button>
                         </div>
                     </DialogPanel>
                 </TransitionChild>
